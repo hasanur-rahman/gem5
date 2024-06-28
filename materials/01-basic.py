1ee1b224a903e90fdf361be0961e20d7eb48ca57
Md Hasanur Rahman <19207216+hasanur-rahman@users.noreply.github.com>
2024-06-28 23:00:10 +0000

    Initial commit

--- materials/01-basic.py
+++ materials/01-basic.py
@@ -1,3 +1,10 @@
 from gem5.prebuilt.demo.x86_demo_board import X86DemoBoard
 from gem5.resources.resource import obtain_resource
 from gem5.simulate.simulator import Simulator
+
+
+board = X86DemoBoard()
+board.set_workload(obtain_resource("x86-ubuntu-18.04-boot"))
+
+simulator = Simulator(board=board)
+simulator.run(max_ticks=10**8)
